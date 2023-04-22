63ebd115172478ef31198ae0c77be454adac0a1f
TranDo25 <73243952+TranDo25@users.noreply.github.com>
2023-04-22 14:22:23 +0700

    viết xong api create order
    chưa test create order

--- src/main/java/com/example/appbanbanhv2/repository/ChiTietOrderRepository.java
+++ src/main/java/com/example/appbanbanhv2/repository/ChiTietOrderRepository.java
@@ -3,6 +3,9 @@ package com.example.appbanbanhv2.repository;
 import com.example.appbanbanhv2.entity.ChiTietOrder;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ChiTietOrderRepository extends JpaRepository<ChiTietOrder, Long> {
+    @Query(value = "SELECT id FROM chi_tiet_order ORDER BY id DESC LIMIT 1;", nativeQuery = true)
+    int getTheLastestId();
 }
--- src/main/java/com/example/appbanbanhv2/service/OrdersServiceImpl.java
+++ src/main/java/com/example/appbanbanhv2/service/OrdersServiceImpl.java
@@ -1,6 +1,10 @@
 package com.example.appbanbanhv2.service;
 
+import com.example.appbanbanhv2.entity.Cart;
+import com.example.appbanbanhv2.entity.ChiTietOrder;
 import com.example.appbanbanhv2.entity.Orders;
+import com.example.appbanbanhv2.entity.Products;
+import com.example.appbanbanhv2.repository.ChiTietOrderRepository;
 import com.example.appbanbanhv2.repository.OrdersRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,14 +13,19 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class OrdersServiceImpl implements OrdersService {
-
+    @Autowired
+    private ProductsService _proService;
     @Autowired
     private OrdersRepository ordersRepository;
+    @Autowired
+    private ChiTietOrderRepository _chiTietOrderRepository;
 
     @Override
     public Orders save(Orders orders) {
@@ -34,18 +43,18 @@ public class OrdersServiceImpl implements OrdersService {
     }
 
     @Override
-    public List<Orders> findAll(Sort sort){
+    public List<Orders> findAll(Sort sort) {
         return ordersRepository.findAll(sort);
     }
 
     @Override
-    public Page<Orders> findAll(Pageable pageable){
+    public Page<Orders> findAll(Pageable pageable) {
         return ordersRepository.findAll(pageable);
     }
 
     @Override
     public void delete(Long id) {
-    ordersRepository.deleteById(id);
+        ordersRepository.deleteById(id);
     }
 
     @Override
@@ -61,6 +70,76 @@ public class OrdersServiceImpl implements OrdersService {
     @Override
     public long count() {
         return ordersRepository.count();
+    }
+
+    @Override
+    public String createOrder(List<Cart> dsCart) {
+        Orders order = new Orders();
+        int count = (int) ordersRepository.count();
+        if (count == 0) {
+            order.setId(1);
+        } else {
+            int theLastestId = (int) ordersRepository.getTheLastestId() + 1;
+            order.setId(theLastestId);
+        }
+
+        LocalDate currentDate = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        order.setNgayTaoDon(currentDate.format(formatter));
+        double tongTienDonHang = 0;
+        for (Cart i : dsCart) {
+            Products tmp = _proService.find(i.getProductsId()).get();
+            double tongTien = tmp.getGia() * i.getSoluong();
+            tongTienDonHang += tongTien;
+        }
+        order.setTongtien(tongTienDonHang);
+        order.setTrangThaiDonHang("created");
+        String res = "";
+        try {
+            ordersRepository.save(order);
+            for (Cart i : dsCart) {
+                ChiTietOrder chiTietOrder = new ChiTietOrder();
+                int countRecordInChiTietOrder = (int) _chiTietOrderRepository.count();
+                if(countRecordInChiTietOrder == 0){
+                    chiTietOrder.setId(1);
+                }
+                else{
+                    int theLastestChiTietOrderId = _chiTietOrderRepository.getTheLastestId()+1;
+                    chiTietOrder.setId(theLastestChiTietOrderId);
+                }
+                chiTietOrder.setCartId(i.getId());
+                chiTietOrder.setOrdersId(order.getId());
+                _chiTietOrderRepository.save(chiTietOrder);
+            }
+
+
+            res = "successful";
+        } catch (Exception e) {
+            res = "error in create order";
+        } return res;
+
+    }
+
+    @Override
+    public String setThongTinGiaoHang(int idOrder, String sdt, String diaChiGiaoHang) {
+        String message = "";
+        Optional<Orders> tmp2 = ordersRepository.findById((long) idOrder);
+        if (tmp2.isPresent()) {
+            Orders tmp = tmp2.get();
+            tmp.setSdt(sdt);
+            tmp.setDiachigiaohang(diaChiGiaoHang);
+
+            try {
+                ordersRepository.save(tmp);
+                message = "set info successful";
+            } catch (Exception e) {
+                message = "error in set info";
+            }
+        } else {
+            message = "cannot find order";
+        }
+
+        return message;
     }
 
 }
